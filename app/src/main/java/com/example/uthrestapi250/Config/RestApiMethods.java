53fd3e20837a5ffca2976e11418895eae66ce3b0
Dell <ricardo.lagoshn@gmail.com>
2025-07-05 17:40:28 -0600

    Initial commit 05/07/2025

---
+++ app/src/main/java/com/example/uthrestapi250/Config/RestApiMethods.java
@@ -0,0 +1,14 @@
+package com.example.uthrestapi250.Config;
+
+public class RestApiMethods {
+
+    // URL base del backend PHP
+    public static final String BASE_URL = "http://192.168.35.216/crud-php/";
+
+    // Endpoints del CRUD
+    public static final String EndpointGetPersons   = BASE_URL + "GetPersons.php";
+    public static final String EndpointCreatePerson = BASE_URL + "PostPersons.php";
+    public static final String EndpointUpdatePerson = BASE_URL + "UpdatePersons.php";
+    public static final String EndpointDeletePerson = BASE_URL + "DeletePersons.php";
+
+}
---
+++ app/src/main/java/com/example/uthrestapi250/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.uthrestapi250;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.RequestQueue;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private RequestQueue requestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
